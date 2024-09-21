9dcb682989aa4fd5c07ddec2b5272be349015e6d
ganesh-shivkumar <ganny131293@gmail.com>
2024-09-21 23:30:35 +0530

    Checking in edi converter which is working for a sample input

--- finetune_script.py
+++ finetune_script.py
@@ -4,7 +4,6 @@ Install the Google AI Python SDK
 $ pip install google-generativeai
 """
 import time
-import os
 import google.generativeai as genai
 from load_creds import load_creds
 
@@ -54,9 +53,3 @@ for status in operation.wait_bar():
 
 result = operation.result()
 print(result)
-
-#genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-
-#model = genai.GenerativeModel(model_name=result.name)
-#response = model.generate_content("ISA*00*          *00*          *01*197819683      *01*00060902413TST *240919*0650*U*00401*000001133*0*T*|~\n GS*PR*197819683*0060902411A2B*240919*0650*000000131*X*004010~\n ST*855*600001482~\n BAK*06*AD*100222*20160922~\n REF*YB*0~\n N9*L1**Notes ~\n MSG*Acknowledge on order received.~\n PO1*1*1999.1*EA***BP*86100000~\n PID*F****Test Product 100001~\n ACK*IA*1999.1*EA*069*20160929~\n N9*L1**Letters or Notes~\n MSG*Acknowledge on order received.~\nCTT*1~\n SE*12*600001482~\n GE*1*000001133~\n IEA*1*0000001133~")
-#print(response.text)
