df456395a4168166cf0e6d752179a57219d55b69
ganesh-shivkumar <ganny131293@gmail.com>
2024-10-27 11:42:45 +0530

    Implementing the API for generic gemini api call

--- edi_converter.py
+++ edi_converter.py
@@ -23,3 +23,11 @@ def get_json_response_for_edi(edi) :
     tuned_prompt = """Convert the given EDI into JSON as per tuned model.""" + edi
     tuned_response = tuned_model.generate_content(tuned_prompt)
     return tuned_response.text
+
+def generic_prompt(edi, json, chat_input) :
+    chat_prompt = """Consider the EDI given below""" + edi + '\n'
+    chat_prompt = chat_prompt + """Consider the EDI given below""" + json + '\n'
+    chat_prompt = chat_prompt + """Give the answer for the questions based on the edi and json passed.""" + chat_input
+    default_flash_model = genai.GenerativeModel("models/gemini-1.5-flash")
+    input_response = default_flash_model.generate_content(chat_prompt)
+    return input_response.text
--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from edi_converter import get_json_response_for_edi, get_latest_from_db
+from edi_converter import get_json_response_for_edi, get_latest_from_db, generic_prompt
 from finetune_script import finetune_model
 
 app = Flask(__name__)
@@ -19,7 +19,11 @@ def get_latest_tuned_model():
 
 @app.route('/api/chat', methods=['POST'])
 def call_gemini():
-    return jsonify("123456")
+    json_request = request.json
+    edi = json_request['edi']
+    json = json_request['json']
+    chat_input = json_request['input']
+    return jsonify(generic_prompt(edi, json, chat_input))
 
 if __name__ == '__main__':
     app.run()
