a72e9ed9821d44f18a1805e4ddb24bd88bd1df23
ganesh-shivkumar <ganny131293@gmail.com>
2024-11-02 19:58:31 +0530

    End point for user create / login

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from edi_converter import get_json_response_for_edi, get_latest_from_db, generic_prompt
 from finetune_script import finetune_model
+from user_login import create_or_login_user
 
 app = Flask(__name__)
 
@@ -24,6 +25,14 @@ def call_gemini():
     json = json_request['json']
     chat_input = json_request['input']
     return jsonify(generic_prompt(edi, json, chat_input))
+
+@app.route('/api/user/login', methods['POST'])
+def create_user():
+    json_request = request.json
+    username = json_request['username']
+    password = json_request['password']
+    action = json_request['action']
+    return jsonify(create_or_login_user(username, password, action))
 
 if __name__ == '__main__':
     app.run()
---
+++ user_login.py
@@ -0,0 +1,49 @@
+from populate_edi855_data import get_database
+import json
+
+edi855_json_db = get_database("edi855_edi_json_data")
+
+def create_or_login_user(username, password, action):
+    if action == 'CREATE':
+        create_user(username, password)
+    else:
+        login_user(username, password)
+
+def create_user(username, password):
+    model_collection = edi855_json_db["user_details"]
+    user_count = model_collection.count_documents({ "username": username })
+    if user_count > 0:
+        message = "User already exists with username : "+username
+        output = '{ "status":"FAILURE", "message": ' + message + '}'
+        return output
+    else:
+        user_details = {
+            "_id" : username,
+            "username" : username,
+            "password" : password
+        }
+        model_collection.insert_one(user_details)
+        message = "User successfully with username : " + username
+        output = '{ "status":"SUCCESS", "message": ' + message + '}'
+        return output
+
+def login_user(username, password):
+    model_collection = edi855_json_db["user_details"]
+    user_count = model_collection.count_documents({ "username": username })
+
+    if user_count > 0:
+        user_rows = model_collection.find({ "username": username })
+        for row in user_rows:
+            if row.get("password") == password:
+                user_exists_message = "Login successful with username : "+username
+                user_exists_output = '{ "status":"SUCCESS", "message": ' + user_exists_message + '}'
+                return user_exists_output
+
+        invalid_password_message = "Invalid Password for username : "+username
+        invalid_password_output = '{ "status":"FAILURE", "message": ' + invalid_password_message + '}'
+        return invalid_password_output
+
+    else:
+        message = "Username doesn't exists : " + username
+        output = '{ "status":"FAILURE", "message": ' + message + '}'
+        return output
